3f86e6af522310d8f23cf11a887d46be4bbe17dd
Henry M. Ceresetti <103071259+hceresetti@users.noreply.github.com>
2023-03-31 19:43:34 -0300

    Update main.java
    No functional changes.

--- Jogo/src/main.java
+++ Jogo/src/main.java
@@ -1,11 +1,11 @@
-public class main {
+public class Main {
 	public static void main(String[] args) {
-		character c1 = new hero(75, 10, "Megazord", intention.EVIL);
-		character c2 = new villain(100, 20, "Lord Zedd", Intencao.GOOD);
+		Character c1 = new hero(75, 10, "Megazord", intention.EVIL);
+		Character c2 = new villain(100, 20, "Lord Zedd", Intencao.GOOD);
 		
-		c1.attack(c);		
-		System.out.println(c);
-		c1.atacar(c);		
-		System.out.println(c);
+		c1.attack(c2);		
+		System.out.println(c1);
+		c2.attack(c1);		
+		System.out.println(c2);
 	}
 }
