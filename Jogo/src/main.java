817c0900ddf4478be65019005caab94112438633
Henry M. Ceresetti <103071259+hceresetti@users.noreply.github.com>
2023-03-31 19:22:25 -0300

    Update main.java
    "hero" and "villain" switched to their correct instances + "c1" attacks, not "c2" for a game just battle.

--- Jogo/src/main.java
+++ Jogo/src/main.java
@@ -1,11 +1,11 @@
 public class main {
 	public static void main(String[] args) {
-		character c = new villain(10, 5, "Megazord", intention.EVIL);
-		character c2 = new hero(10, 6, "Lord Zedd", Intencao.GOOD);
+		character c1 = new hero(75, 10, "Megazord", intention.EVIL);
+		character c2 = new villain(100, 20, "Lord Zedd", Intencao.GOOD);
 		
-		c2.attack(c);		
+		c1.attack(c);		
 		System.out.println(c);
-		c2.atacar(c);		
+		c1.atacar(c);		
 		System.out.println(c);
 	}
 }
